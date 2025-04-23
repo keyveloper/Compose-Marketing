64d393540efed800c8737da9405c0071851ea7a9
zen <didehdrb21@naver.com>
2025-04-23 11:07:08 +0900

    show different screen user & advertiser

--- app/src/main/java/com/example/marketing/ui/component/bar/MainBottomBar.kt
+++ app/src/main/java/com/example/marketing/ui/component/bar/MainBottomBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.ui.*
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Key
@@ -15,11 +16,13 @@ import androidx.compose.ui.graphics.*
 import androidx.navigation.NavController
 import com.example.marketing.enums.MainScreenStatus
 import com.example.marketing.enums.ScreenRoute
+import com.example.marketing.enums.UserStatus
 
 @Composable
 fun MainBottomBar(
     modifier: Modifier = Modifier,
-    onStatusChange: (MainScreenStatus) -> Unit
+    onStatusChange: (MainScreenStatus) -> Unit,
+    userStatus: UserStatus
 ) {
     Box(
         modifier = modifier
@@ -59,15 +62,36 @@ fun MainBottomBar(
                 )
             }
 
-            IconButton(
-                onClick = { onStatusChange(MainScreenStatus.FOLLOW) }
-            ) {
-                Icon(
-                    imageVector = Icons.Outlined.FavoriteBorder,
-                    contentDescription = "my advertisement",
-                    tint = Color.Gray
-                )
+
+            // change when advertiser
+            when (userStatus) {
+                UserStatus.INFLUENCER -> {
+                    IconButton(
+                        onClick = { onStatusChange(MainScreenStatus.FOLLOW) }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.FavoriteBorder,
+                            contentDescription = "my advertisement",
+                            tint = Color.Gray
+                        )
+                    }
+                }
+
+                UserStatus.ADVERTISER -> {
+                    IconButton(
+                        onClick = { onStatusChange(MainScreenStatus.ADVERTISER_WRITE) }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Edit,
+                            contentDescription = "advertiser write Icon for advertiser",
+                            tint = Color.Gray
+                        )
+                    }
+                }
+
+                else -> {}
             }
+
 
             IconButton(
                 onClick = { onStatusChange(MainScreenStatus.PROFILE) }
